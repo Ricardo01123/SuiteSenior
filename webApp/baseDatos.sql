8f59d4717895d5f01d7d36548d6ffa88569fda2d
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2023-04-12 14:43:08 -0600

    microcommit: cambio a la estructura de la base de datos

--- webApp/baseDatos.sql
+++ webApp/baseDatos.sql
@@ -7,6 +7,10 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
+DROP DATABASE IF EXISTS `SeniorSuite`;
+CREATE DATABASE `SeniorSuite` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `SeniorSuite`;
+
 DROP TABLE IF EXISTS `Notas_medicas`;
 CREATE TABLE `Notas_medicas` (
   `id_nota` int NOT NULL AUTO_INCREMENT,
@@ -17,8 +21,6 @@ CREATE TABLE `Notas_medicas` (
   CONSTRAINT `Notas_medicas_ibfk_1` FOREIGN KEY (`No_Expediente`) REFERENCES `Paciente` (`No_Expediente`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `Notas_medicas` (`id_nota`, `No_Expediente`, `Nota`) VALUES
-(5,	55576003,	'El paciente se ha sesion pepe');
 
 DROP TABLE IF EXISTS `Paciente`;
 CREATE TABLE `Paciente` (
@@ -28,7 +30,7 @@ CREATE TABLE `Paciente` (
   `Sexo` int NOT NULL,
   `Padecimiento` int NOT NULL,
   `Telefono` bigint NOT NULL,
-  `Foto` varchar(100) DEFAULT NULL,
+  `Foto` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `Otros Contactos` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`No_Expediente`),
   KEY `Padecimiento` (`Padecimiento`),
@@ -42,8 +44,6 @@ CREATE TABLE `Paciente` (
   CONSTRAINT `Paciente_ibfk_7` FOREIGN KEY (`Sexo`) REFERENCES `Sexo` (`Sexo`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `Paciente` (`No_Expediente`, `Nombre`, `Edad`, `Sexo`, `Padecimiento`, `Telefono`, `Foto`, `Otros Contactos`) VALUES
-(55576003,	'pepe',	105,	1,	2,	5566554433,	NULL,	NULL);
 
 DROP TABLE IF EXISTS `Padecimiento`;
 CREATE TABLE `Padecimiento` (
@@ -55,7 +55,8 @@ CREATE TABLE `Padecimiento` (
 INSERT INTO `Padecimiento` (`Padecimiento`, `Valor_Padecimiento`) VALUES
 (1,	'Estrés'),
 (2,	'Depresion'),
-(3,	'Ansiedad');
+(3,	'Ansiedad'),
+(4,	'Estrés postraumático');
 
 DROP TABLE IF EXISTS `Rol`;
 CREATE TABLE `Rol` (
@@ -78,8 +79,6 @@ CREATE TABLE `Sesiones_diarias` (
   CONSTRAINT `Sesiones_diarias_ibfk_1` FOREIGN KEY (`No_Expediente`) REFERENCES `Paciente` (`No_Expediente`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `Sesiones_diarias` (`id_sesion`, `No_Expediente`, `Resumen`) VALUES
-(2,	55576003,	'Hola como has estado Alexa, este dia a sido interesante para mí, mi nieto vino a visitarme y me sentí muy contenta, el dia fué realmente agradable para mi., este es el resmuen de pepe');
 
 DROP TABLE IF EXISTS `Sexo`;
 CREATE TABLE `Sexo` (
@@ -107,4 +106,4 @@ INSERT INTO `Usuario` (`id_usuario`, `usuario`, `contraseña`, `rol`) VALUES
 (1,	'jacob',	'jacob',	1),
 (2,	'Bety',	'Bety',	2);
 
--- 2023-04-07 06:10:27
+-- 2023-04-12 20:42:12
